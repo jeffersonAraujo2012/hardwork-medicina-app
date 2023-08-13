737f6d01a244fdc41402a49d75718fa838e80f9c
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-08-13 15:23:49 -0300

    Refact: App/questions/page.tsx

--- app/questions/page.tsx
+++ app/questions/page.tsx
@@ -1,19 +1,18 @@
 import QuestionSelector from "@/components/Question/QuestionSelector";
 import HeaderPage from "../../components/HeaderPage";
 import Question from "../../components/Question";
-import s from "./styles.module.scss";
 import { GetQuestionsAsync } from "@/services/questionsService";
 
 export default async function QuestionsPage() {
-  const questions = await GetQuestionsAsync();
+  const {obj: questions} = await GetQuestionsAsync();
   
   return (
     <main className="mainContainer flex flex-col">
       <HeaderPage pageTitle="Questões" />
 
-      <Question questions={questions.obj}/>
+      <Question questions={questions}/>
 
-      <QuestionSelector questions={questions.obj} />
+      <QuestionSelector questions={questions} />
     </main>
   );
 }
