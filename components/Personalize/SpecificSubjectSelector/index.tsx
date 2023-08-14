dd9974a108b6eb5ddbfb9cc1d45ee3f1aee15753
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-08-13 21:38:22 -0300

    Add: SpecificSubjectSelector component created.

---
+++ components/Personalize/SpecificSubjectSelector/index.tsx
@@ -0,0 +1,29 @@
+import { Autocomplete, TextField } from "@mui/material";
+import Container from "../Container";
+
+const specificSubjectsMock = [
+  { label: "ABC" },
+  { label: "AABC" },
+  { label: "ABBC" },
+  { label: "AAABCC" },
+];
+
+export default function SpecificSubjectSelector() {
+  return (
+    <Container description="Questões de temas específicos:">
+      <Autocomplete
+        disablePortal
+        id="temas"
+        options={specificSubjectsMock}
+        renderOption={(props, option) => {
+          return (
+            <li {...props} key={option.label}>
+              {option.label}
+            </li>
+          );
+        }}
+        renderInput={(params) => <TextField {...params} label="Temas" />}
+      />
+    </Container>
+  );
+}
