e12b4bf7af79bcb21b16c295db8c7a957996014d
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-08-13 21:39:07 -0300

    Add: TestSelector component created.

---
+++ components/Personalize/TestSelector/index.tsx
@@ -0,0 +1,29 @@
+import { Autocomplete, TextField } from "@mui/material";
+import Container from "../Container";
+
+const provasMock = [
+  { label: "ABC" },
+  { label: "AABC" },
+  { label: "ABBC" },
+  { label: "AAABCC" },
+];
+
+export default function TestSelector() {
+  return (
+    <Container description="Questões de provas específicas:">
+      <Autocomplete
+        disablePortal
+        id="provas"
+        options={provasMock}
+        renderOption={(props, option) => {
+          return (
+            <li {...props} key={option.label}>
+              {option.label}
+            </li>
+          );
+        }}
+        renderInput={(params) => <TextField {...params} label="Provas" />}
+      />
+    </Container>
+  );
+}
